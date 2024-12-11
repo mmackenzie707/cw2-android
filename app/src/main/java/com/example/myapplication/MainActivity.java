6886d963c8714aba017352e04395574c97b3d106
Micheal Mackenzie <20170200@learner.hkuspace.hku.hk>
2024-12-12 00:03:56 +0800

    1st run

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toolbar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.textBox), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        
+
+        Button btnUserDetails = findViewById(R.id.userDetails);
+        btnUserDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UserPreferencesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnUserHoliday = findViewById(R.id.userHoliday);
+        btnUserHoliday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, user_holiday.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnUserSettings = findViewById(R.id.userSettings);
+        btnUserSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/userNotifications.java
@@ -0,0 +1,6 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+
+public class userNotifications extends Activity {
+}
