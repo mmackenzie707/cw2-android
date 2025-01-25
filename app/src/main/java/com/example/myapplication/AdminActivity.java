07faab98a7201c025692ecde80ec6a417bc6438c
Micheal Mackenzie <20170200@learner.hkuspace.hku.hk>
2025-01-25 17:11:19 +0800

    14th commit

--- app/src/main/java/com/example/myapplication/AdminActivity.java
+++ app/src/main/java/com/example/myapplication/AdminActivity.java
@@ -1,109 +1,84 @@
 package com.example.myapplication;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
-import android.widget.Toast;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import java.util.List;
 
 public class AdminActivity extends AppCompatActivity {
 
-    ListView userListView;
-    ArrayAdapter<String> userAdapter;
-    List<String> userList;
-    Button userMaintenanceButton;
-    Button holidayCenterButton; // Holiday Center button
-    Button privacyPolicyButton; // Privacy Policy button
+    Button buttonEmployeeSubmit, adminbtn_holidayCenter, adminbtn_adminprofile, adminbtn_privacyPolicy;
+    BottomNavigationView bottomNavigationView;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
 
-        userListView = findViewById(R.id.userListView);
-        userMaintenanceButton = findViewById(R.id.adminbtn_holidayCenter);
-        Button logoutButton = findViewById(R.id.logoutButton);
-        Button employeeSubmitButton = findViewById(R.id.buttonEmployeeSubmit);
-        Button adminSettingsButton = findViewById(R.id.adminbtn_adminprofile);
-        holidayCenterButton = findViewById(R.id.adminbtn_holidayCenter); // Ensure this button is in your layout
-        privacyPolicyButton = findViewById(R.id.privacyPolicyButton); // Ensure this button is in your layout
+        buttonEmployeeSubmit = findViewById(R.id.buttonEmployeeSubmit);
+        adminbtn_holidayCenter = findViewById(R.id.adminbtn_holidayCenter);
+        adminbtn_adminprofile = findViewById(R.id.adminbtn_adminprofile);
+        adminbtn_privacyPolicy = findViewById(R.id.adminbtn_privacyPolicy);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        userMaintenanceButton.setOnClickListener(new View.OnClickListener() {
+        buttonEmployeeSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    userList = UserFactory.getUserList();
-                    if (userList == null) {
-                        throw new NullPointerException("User list is null");
-                    }
-                    userAdapter = new ArrayAdapter<>(AdminActivity.this, android.R.layout.simple_list_item_1, userList);
-                    userListView.setAdapter(userAdapter);
-                    userListView.setVisibility(View.VISIBLE);
-                } catch (Exception e) {
-                    Log.e("AdminActivity", "Error setting user list", e);
-                    Toast.makeText(AdminActivity.this, "Error loading user list", Toast.LENGTH_SHORT).show();
+                Intent employeesubmitIntent = new Intent(AdminActivity.this, SubmitEmployeeActivity.class);
+                startActivity(employeesubmitIntent);
+            }
+        });
+
+        adminbtn_holidayCenter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent holidayIntent = new Intent(AdminActivity.this, HolidayRequestActivity.class);
+                startActivity(holidayIntent);
+            }
+        });
+
+        adminbtn_adminprofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent adminIntent = new Intent(AdminActivity.this, AdminSettingsActivity.class);
+                startActivity(adminIntent);
+            }
+        });
+
+        adminbtn_privacyPolicy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent privacyIntent = new Intent(AdminActivity.this, PrivacyPolicyActivity.class);
+                startActivity(privacyIntent);
+            }
+        });
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_dashboard:
+                        Intent dashboardIntent = new Intent(AdminActivity.this, AdminActivity.class);
+                        startActivity(dashboardIntent);
+                        return true;
+                    case R.id.navigation_settings:
+                        Intent settingsIntent = new Intent(AdminActivity.this, AdminSettingsActivity.class);
+                        startActivity(settingsIntent);
+                        return true;
+                    case R.id.navigation_logout:
+                        com.example.myapplication.utils.LogoutUtils.logout(AdminActivity.this);
+                        Intent mainIntent = new Intent(AdminActivity.this, MainActivity.class);
+                        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(mainIntent);
+                        finish();
+                        return true;
                 }
+                return false;
             }
         });
-
-        logoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                com.example.myapplication.utils.LogoutUtils.logout(AdminActivity.this);
-            }
-        });
-
-        employeeSubmitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(AdminActivity.this, SubmitEmployeeActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        adminSettingsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(AdminActivity.this, AdminSettingsActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        holidayCenterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(AdminActivity.this, HolidayRequestActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        privacyPolicyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(AdminActivity.this, PrivacyPolicyActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        // Setup bottom navigation
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        User currentUser = getCurrentUser();
-        NavigationUtils.setupBottomNavigationView(this, bottomNavigationView, currentUser);
-    }
-
-    private User getCurrentUser() {
-        UserSessionManager sessionManager = new UserSessionManager(this);
-        return sessionManager.getCurrentUser();
     }
 }
